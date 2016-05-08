2cc6eda7cc1da55b265fb320ba045f171c68c5fa
linoleum-js <this.frontend@gmail.com>
2016-05-08 11:12:53 +0500

    init

---
+++ src/main.ts
@@ -0,0 +1,4 @@
+
+import PurePlayer from "./player.ts";
+
+const player = new PurePlayer();
---
+++ src/player.ts
@@ -0,0 +1,14 @@
+
+import Track from './track.ts'
+
+/**
+ *
+ */
+export default class PurePlayer {
+  /**
+   *
+   */
+  public play(track: Track) {
+
+  }
+}
